fd25f8e0ddbe1d97d8f03a04613a087ef816d627
Bruno Pachesen <brunopachesen@gmail.com>
2018-10-11 17:14:51 -0300

    definição de rotas

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,57 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//pagina inicial
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-    Route::group(['prefix' => 'administrativo'], function() {
-        
-        Route::get('avisos', function () {
+
+    //menu administrativo
+Route::prefix('administrativo')->group(function () {
+    
+    //pagina inicial administrativo
+    Route::get('/', function () {
+        return view('administrativo/index');
+    });
+    
+    //submenu aluno
+    Route::prefix('aluno')->group(function () {
+            
+        //pagina inicial aluno
+        Route::get('/', function () {
+            return view('administrativo/alunos/index');
+        });
+           
+    });
+    
+    //submenu responsavel
+    Route::prefix('responsavel')->group(function () {
+     
+    //pagina inicial responsavel
+        Route::get('/', function () {
+            return view('administrativo/responsaveis/index');
+        });
+                
+    });
+    
+    //submenu colaborador
+    Route::prefix('colaborador')->group(function () {
+            
+        //pagina inicial colaborador
+        Route::get('/', function () {
+            return view('administrativo/colaboradores/index');
+        });
+                
+    });
+    
+    //submenu avisos
+    Route::prefix('avisos')->group(function () {
+            
+        //pagina inicial avisos
+        Route::get('/', function () {
             return view('administrativo/avisos/index');
         });
+    
     });
+});
